c1492482d8e04848859e0a342e8f316ce6e4f61c
31191025723 <91664686+duyyfinal@users.noreply.github.com>
2023-10-30 23:41:43 +0700

    30/10/23 commit

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,28 +2,60 @@ import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react';
 
+
 import images from '../../../../assets/images';
 import Button from '../../../Button';
+import Menu from '../Menu';
 
 const cs = classNames.bind(styles)
-
-
-
-
 
 function Header() {
     return <div className={cs('wrapper-h')}>
             <div className={cs('nav-wrapper')}>  
                 <div className={cs('left-menu')}>  
+                
                     <img className={cs('logo-he')} src={images.logo} alt='logo'/>
-                    <a href='/'><img className={cs('ic-new')} src={images.icNew} alt='ic new'/></a>
-                    <Button>Bài Hát</Button>
-                    <Button>Playlist</Button>
-                    <Button>Tuyển Tập</Button>
-                    <Button>Video</Button>
-                    <Button>BXH</Button>
-                    <Button>Top 100</Button>
-                    <Button><img className={cs('ic-more')} src={images.icMore} alt='icon more'/></Button>
+                    <a className={cs('new-version')} href='/'><img className={cs('ic-new')} src={images.icNew} alt='ic new'/></a>
+                    <Tippy content="Bài Hát" delay={[1000,1000]}
+                        render={attrs =>(
+                            <Menu />
+                        )}
+                    >    
+                        <a className={cs('option-left-menu')} href='/'>    
+                            <Button>Bài Hát</Button>
+                        </a>
+                    </Tippy>
+                    <Tippy content="Playlist">    
+                        <a className={cs('option-left-menu')} href='/'>    
+                            <Button>Playlist</Button>
+                        </a>
+                    </Tippy>
+                    <Tippy content="Tuyển tập">    
+                        <a className={cs('option-left-menu')} href='/'>    
+                            <Button>Tuyển tập</Button>
+                        </a>
+                    </Tippy>
+                    <Tippy content="Video">    
+                        <a className={cs('option-left-menu')} href='/'>    
+                            <Button>Video</Button>
+                        </a>
+                    </Tippy>
+                    <Tippy content="BXH">    
+                        <a className={cs('option-left-menu')} href='/'>    
+                            <Button>BXH</Button>
+                        </a>
+                    </Tippy>
+                    <Tippy content="Top 100">    
+                        <a className={cs('option-left-menu')} href='/'>    
+                            <Button>Top 100</Button>
+                        </a>
+                    </Tippy>
+                    <Tippy content="Khác">    
+                        <a className={cs('option-left-menu')} href='/'>    
+                        <Button><img className={cs('ic-more')} src={images.icMore} alt='icon more'/></Button>
+                        </a>
+                    </Tippy>
+                    
                 </div>
                 
                 <div className={cs('right-menu')}>
@@ -42,6 +74,7 @@ function Header() {
                     <div className={cs('line')}></div>
                     <Button preprimary>Đăng nhập</Button>
                     <Button primary>Đăng ký</Button>
+                    
                 </div>
             </div>
          </div>;
--- src/components/Layout/components/Sidebar/index.js
+++ src/components/Layout/components/Sidebar/index.js
@@ -3,9 +3,20 @@ import styles from "./Sidebar.module.scss";
 
 const cs = classNames.bind(styles);
 
+var myDog = {
+    'name': 'Ngáo',
+    'legs': 4,
+    'friends':['everything!']
+}
+    myDog.Color = "Brown"
+ delete myDog.friends
+
+ console.log(myDog);
+
 function Sidebar() {
 
     return <div className={cs('f-info')}>
+        
         
     </div>;
 }
